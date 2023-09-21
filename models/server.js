5317574e1eb443fcd89392cdc95bb6bcffd9fe73
Jaime Tapia <jaime.tapia.miquel@gmail.com>
2023-09-21 12:26:37 -0300

    App Chat con Sockets

--- models/server.js
+++ models/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const { createServer } = require('http');
 
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/socket.controller');
 
 class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT;
+		this.server = createServer(this.app);
+		this.io = require('socket.io')(this.server);
 
 		this.paths = {
 			auth: '/api/auth',
@@ -26,6 +30,9 @@ class Server {
 
 		// Rutas de mi aplicación
 		this.routes();
+
+		// Sockets
+		this.sockets();
 	}
 
 	async conectarDB() {
@@ -67,8 +74,12 @@ class Server {
 		this.app.use(this.paths.uploads, require('../routes/uploads.routes'));
 	}
 
+	sockets() {
+		this.io.on('connection', (socket) => socketController(socket, this.io));
+	}
+
 	listen() {
-		this.app.listen(this.port, () => {
+		this.server.listen(this.port, () => {
 			console.log('Servidor corriendo en puerto', this.port);
 		});
 	}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login, googleSignIn } = require('../controllers/auth.controller');
-const { validarCampos } = require('../middlewares/validar-campos');
+const {
+	login,
+	googleSignIn,
+	renovarToken,
+} = require('../controllers/auth.controller');
+const { validarCampos, validarJWT } = require('../middlewares');
 
 const router = Router();
 
@@ -24,5 +28,7 @@ router.post(
 	],
 	googleSignIn
 );
+
+router.get('/', validarJWT, renovarToken);
 
 module.exports = router;
